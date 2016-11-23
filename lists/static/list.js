3bb05f9a17724fa1a5915cd9594383a76299d610
Chris Moore <cwm322@gmail.com>
2016-11-23 20:47:25 +0000

    javascript unit teting using Qunit

--- lists/static/list.js
+++ lists/static/list.js
@@ -1,7 +1,8 @@
-var initialize = function(){
+window.Superlists = {};
+
+
+window.Superlists.initialize = function(){
 	$('input[name="text"]').on('keypress', function(){
 		$('.has-error').hide();	
 	});
 }
-
-
